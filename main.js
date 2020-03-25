fa3dee4d246a1dc891da76b99a87ad25ed74cc1c
Wilson Chen <wilson.chen@codeimmersives.com>
2020-03-24 21:44:47 -0400

    finish

--- main.js
+++ main.js
@@ -1,10 +1,71 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const triple = function(num){
+  return num * 3
+}
 
+const tripleAll = function(array){
+  return array.map(triple)
+}
 
+const getNegativeValue = function(num){
+  return num > 0 ? -1 * num : num
 
+}
 
+const negativeValues = function(array){
+  return array.map(getNegativeValue)
+}
+
+const makeQuestion = function(str){
+  return str + '?'
+}
+
+const questionings = function(array){
+  return array.map(makeQuestion)
+}
+
+const getInitials = function(str){
+  for (let i = 0;i < str.length; i++){
+    return str[0]+str[str.indexOf(' ')+1]
+  }
+}
+
+const changeToInitials = function(array){
+  return array.map(getInitials)
+}
+
+const doubleIfEven = function(num){
+  return num % 2 === 0 ? num *2 : num
+}
+
+const doubleEven = function(array){
+  return array.map(doubleIfEven)
+}
+
+const titleCase = function(str){
+  let result = ''
+  for (let i = 0;i < str.length; i++){
+    result+=(i===0 ? str[i].toUpperCase(): str[i].toLowerCase())
+}return result
+}
+
+const titleCaseNames = function(array){
+  return array.map(titleCase)
+}
+
+const doubleEveryOther = function(array){
+  let result = []
+  for (let i = 0;i < array.length; i++){
+    result.push(i % 2 ===0 || i ===0 ? array[i]: array[i]*2)
+} return result
+}
+// const doubleEveryOther =function(array){
+
+//   return array.map(doubleIfEven(array.index)
+
+// }
 /********************************
  * OUR CODE HERE - DON'T TOUCH! *
  ********************************/
